10f90d878c4781fb0a834343c794884848959cde
Jason Cress <jcressmuse@gmail.com>
2024-02-12 12:35:14 -0600

    modified start/stop script

--- python/start-datachannel.sh
+++ python/start-datachannel.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 
-/usr/local/bin/python /u01/IBM/sevone-watson-datachannel-main/python/sevone-watson-datachannel.py &
+/usr/local/bin/python zabbix-aiops-metric-connector.py &
--- python/stop-datachannel.sh
+++ python/stop-datachannel.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PID=`ps -ef |grep sevone-watson-datachannel | grep -v grep | awk '{print $2}'`
+PID=`ps -ef |grep zabbix-aiops-metric-connector | grep -v grep | awk '{print $2}'`
 echo $PID
 
 [[ ! -z "$PID" ]] && kill -9 $PID || echo "Not running"
